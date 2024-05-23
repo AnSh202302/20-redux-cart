80c4b53c822778059d7abfd6387b8f246aed5ea5
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2024-05-23 10:12:20 +0200

    add cart-slice

---
+++ src/store/cart-slice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialCartState = {
+  items: [],
+  totalQuantity: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: initialCartState,
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItems = state.items.find((item) => item.id === newItem.id);
+
+      if (existingItems) {
+        existingItems.quantity++;
+        existingItems.totalPrice = existingItems.totalPrice + newItem.price;
+      } else {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      }
+    },
+    remuveItemToCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+      }
+    },
+  },
+});
+
+export const CartActions = cartSlice.actions;
+
+export default cartSlice.reducer;
